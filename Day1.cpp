77ce5406814e498c100a2ae9c863123f76bfb42c
archisonal <sonalarchi21@gmail.com>
2025-03-18 03:37:25 +0530

    PS 1

--- Day1.cpp
+++ Day1.cpp
@@ -1,6 +1,13 @@
+// Problem Statement-1
+// Write a C++ Program to enter the any text and in next line display the same text which is entered by the user using the functions "cin" and "cout". 
+// You are to take reference of below testcases and solve accordingly
+
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hey World";
+    string data;
+    cout<<"Enter the text: ";
+    getline(cin, data);
+    cout<<data<<endl;
     return 0;
 }
